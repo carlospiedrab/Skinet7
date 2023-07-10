309afa8d56581a7b1ea69953cb5ef97f45e5bc9c
Carlos Piedra <barce90@hotmail.com>
2023-07-09 20:31:05 -0700

    End Of Section 13

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -7,6 +7,7 @@ using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
 
 namespace API.Extensions
 {
@@ -22,7 +23,12 @@ namespace API.Extensions
 
             services.AddDbContext<StoreContext>(options =>
                 options.UseSqlServer(connectionString));
-
+            services.AddSingleton<IConnectionMultiplexer>(c =>
+            {
+                var options = ConfigurationOptions.Parse(config.GetConnectionString("Redis"));
+                return ConnectionMultiplexer.Connect(options);
+            });
+            services.AddScoped<IBasketRepository, BasketRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
